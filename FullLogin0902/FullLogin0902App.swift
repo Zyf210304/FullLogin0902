b22ccd2c2b99a3ee87c2904c221bc58998b9954b
张亚飞 <672233580@qq.com>
2021-09-02 09:36:36 +0800

    Initial Commit

---
+++ FullLogin0902/FullLogin0902App.swift
@@ -0,0 +1,17 @@
+//
+//  FullLogin0902App.swift
+//  FullLogin0902
+//
+//  Created by 张亚飞 on 2021/9/2.
+//
+
+import SwiftUI
+
+@main
+struct FullLogin0902App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
